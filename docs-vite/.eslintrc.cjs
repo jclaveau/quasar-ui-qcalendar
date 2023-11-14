88ce02f4fbaba85a0c9b43a7a42f4d60d1f1d4dd
Jean Claveau <jean.claveau@gmail.com>
2023-11-14 11:03:04 +0100

    feat(vite): merge wabpack based docs into docs-vite

--- docs-vite/.eslintrc.cjs
+++ docs-vite/.eslintrc.cjs
@@ -6,12 +6,16 @@ module.exports = {
 
   parserOptions: {
     ecmaVersion: 2021, // Allows for the parsing of modern ECMAScript features
+    parser: '@babel/eslint-parser',
+    sourceType: 'module', // Allows for the use of imports
+    requireConfigFile: false
   },
 
   env: {
     node: true,
     browser: true,
     'vue/setup-compiler-macros': true
+    es6: true
   },
 
   // Rules order is important, please avoid shuffling them
@@ -22,22 +26,21 @@ module.exports = {
     // Uncomment any of the lines below to choose desired strictness,
     // but leave only one uncommented!
     // See https://eslint.vuejs.org/rules/#available-rules
-    'plugin:vue/vue3-essential', // Priority A: Essential (Error Prevention)
+    // 'plugin:vue/vue3-essential' // Priority A: Essential (Error Prevention)
     // 'plugin:vue/vue3-strongly-recommended', // Priority B: Strongly Recommended (Improving Readability)
-    // 'plugin:vue/vue3-recommended', // Priority C: Recommended (Minimizing Arbitrary Choices and Cognitive Overhead)
-
+    'plugin:vue/vue3-recommended', // Priority C: Recommended (Minimizing Arbitrary Choices and Cognitive Overhead)
     'standard'
-    
   ],
 
   plugins: [
     // https://eslint.vuejs.org/user-guide/#why-doesn-t-it-work-on-vue-files
     // required to lint *.vue files
-    'vue',
-    
+    'vue'
   ],
 
   globals: {
+    $q: 'readonly',
+    $router: 'readonly',
     ga: 'readonly', // Google Analytics
     cordova: 'readonly',
     __statics: 'readonly',
@@ -47,32 +50,57 @@ module.exports = {
     __QUASAR_SSR_PWA__: 'readonly',
     process: 'readonly',
     Capacitor: 'readonly',
-    chrome: 'readonly'
+    chrome: 'readonly',
   },
 
   // add your custom rules here
   rules: {
-    
-    // allow async-await
+    'brace-style': [ 'error', 'stroustrup', { allowSingleLine: true } ],
+    'prefer-const': 'error',
+    'prefer-promise-reject-errors': 'off',
+    'multiline-ternary': 'off',
+    'no-prototype-builtins': 'off',
+    'no-case-declarations': 'off',
     'generator-star-spacing': 'off',
-    // allow paren-less arrow functions
     'arrow-parens': 'off',
+    'object-property-newline': 'off',
     'one-var': 'off',
     'no-void': 'off',
-    'multiline-ternary': 'off',
+    'no-lone-blocks': 'error',
+    'no-unused-expressions': [ 'error', { allowTernary: true, "allowShortCircuit": true } ],
+    'no-useless-concat': 'error',
+    'no-useless-return': 'error',
+    'no-unneeded-ternary': 'error',
+    'no-confusing-arrow': [ 'error', { allowParens: true } ],
+    'operator-linebreak': [ 'error', 'before' ],
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+
+    'array-bracket-spacing': [ 'error', 'always', { singleValue: false } ],
+    'object-curly-spacing': [ 'error', 'always' ],
+    'computed-property-spacing': [ 'error', 'always' ],
+    'template-curly-spacing': [ 'error', 'always' ],
 
     'import/first': 'off',
-    'import/named': 'error',
-    'import/namespace': 'error',
-    'import/default': 'error',
-    'import/export': 'error',
+    // 'import/named': 'error',
+    // 'import/namespace': 'error',
+    // 'import/default': 'error',
+    // 'import/export': 'error',
     'import/extensions': 'off',
     'import/no-unresolved': 'off',
     'import/no-extraneous-dependencies': 'off',
-    
-    'prefer-promise-reject-errors': 'off',
 
-    // allow debugger during development only
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'import/no-webpack-loader-syntax': 'off',
+
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/no-multiple-template-root': 'off',
+    'vue/multi-word-component-names': 'off',
+    'vue/require-default-prop': 'off',
+    'vue/no-v-html': 'off',
+    'vue/require-prop-types': 'off',
+    'vue/valid-v-slot': 'off',
+
+    // allow console.log during production for demo purposes
+    // 'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-console': 'off'
   }
 }
